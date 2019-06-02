cbf6c4becbee8a2918473116b588b976cad8f3d5
Artem Tsygankov <acigankov@inbox.ru>
2019-06-02 23:17:08 +0300

    добавлены вопросы к играм ВК_1 и МАРВЕЛ_2

--- config/db_params.php
+++ config/db_params.php
@@ -1,14 +1,11 @@
 <?php
 
 return array(
-//    'host'      => 'localhost',
-//    'dbname'    => 'quiz_copy_prod',
-//    'user'      => 'root',
-//    'password'  => ''
-    'host'      => 'mysql-105942.srv.hoster.ru',
-    'dbname'    => 'srv105942_quiz',
-    'user'      => 'srv105942_quiz',
-    'password'  => '7kZnAkUx'
+    'host'      => 'localhost',
+    'dbname'    => 'quiz_copy_prod',
+    'user'      => 'root',
+    'password'  => ''
+
 
 );
 
